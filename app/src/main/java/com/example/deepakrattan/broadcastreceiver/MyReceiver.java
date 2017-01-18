dff3b5467dda3c9232ab09f5f5e8a654478e710e
Deepak Rattan <mca.deepakrattan@gmail.com>
2017-01-18 15:36:06 -0800

    Initial commit

---
+++ app/src/main/java/com/example/deepakrattan/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.deepakrattan.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by deepak.rattan on 1/4/2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Airplane Mode changed", Toast.LENGTH_LONG).show();
+    }
+}
